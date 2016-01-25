fa9e2f90bda23c53ec05676f990474284aabc1a8
lysu <sulifx@gmail.com>
2016-01-26 00:31:54 +0800

    docs(saga):Fix typo question

--- coordinator_test.go
+++ coordinator_test.go
@@ -2,26 +2,26 @@ package saga
 
 import "golang.org/x/net/context"
 
-// This example show how to use init a SEC and add Sub-transaction to it, finally trigger a transfer operation as demo.
-// In a saga transaction we deduce `100` from foo at first, then deposit 100 into `bar`.
+// This example show how to initialize an Saga execution coordinator(SEC) and add Sub-transaction to it, then start a transfer transaction.
+// In transfer transaction we deduce `100` from foo at first, then deposit 100 into `bar`, deduce & deduce wil both success or rollbacked.
 func ExampleExecutionCoordinator_transfer() {
 
 	// 1. Define sub-transaction method, anonymous method is NOT required, Just define them as normal way.
 
 	DeduceAccount := func(ctx context.Context, account string, amount int) error {
-		// Do deduce amount from account
+		// Do deduce amount from account, like: account.money - amount
 		return nil
 	}
 	CompensateDeduce := func(ctx context.Context, account string, amount int) error {
-		// Compensate deduce amount to account
+		// Compensate deduce amount to account, like: account.money + amount
 		return nil
 	}
 	DepositAccount := func(ctx context.Context, account string, amount int) error {
-		// Do deposit amount to account
+		// Do deposit amount to account, like: account.money + amount
 		return nil
 	}
 	CompensateDeposit := func(ctx context.Context, account string, amount int) error {
-		// Compensate deposit amount from account
+		// Compensate deposit amount from account, like: account.money - amount
 		return nil
 	}
 
